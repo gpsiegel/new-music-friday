608e6bfab3492767029b2cda967616da1915e1ca
Grant <gpsiegel@hotmail.com>
2021-03-10 14:26:16 -0600

    Update test_load.py
    updated with classes

--- test_load.py
+++ test_load.py
@@ -4,28 +4,26 @@ import json
 
 from id import *
 
-country = 'US'
-limit = 10
+class SpotifyAuth:
 
-token_url = 'https://accounts.spotify.com/api/token'
+    def __init__(self, client_id, client_secret):
+        self.client_id = client_id
+        self.client_secret = client_secret
 
-new_rel_url = f'https://api.spotify.com/v1/browse/new-releases?country={country}&limit={limit}'
+    def server_auth(self, country='US', limit=10):
+        token_url = 'https://accounts.spotify.com/api/token'
+        new_rel_url = f'https://api.spotify.com/v1/browse/new-releases?country={country}&limit={limit}'
 
-data = {'grant_type': 'client_credentials'}
+        data = {'grant_type': 'client_credentials'}
+        token_response = requests.post(token_url, data=data, \
+            verify=True, allow_redirects=False, \
+            auth=(client_id, client_secret))
 
+        tokens = json.loads(token_response.text)
 
-token_response = requests.post(token_url, data=data, \
-    verify=True, allow_redirects=False, \
-    auth=(client_id, client_secret))
+        access_token = tokens['access_token']
 
-print(token_response.headers)
-print(token_response.text)
+        api_headers = {'Authorization': f'Bearer {access_token}'}
+        api_response = requests.get(new_rel_url, headers=api_headers, verify=True)
 
-tokens = json.loads(token_response.text)
-
-access_token = tokens['access_token']
-
-api_headers = {'Authorization': f'Bearer {access_token}'}
-api_response = requests.get(new_rel_url, headers=api_headers, verify=True)
-
-print(api_response.text)
+        print(api_response.text)
